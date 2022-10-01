8eb550539b330d9347737e43dc7175d8036f33e3
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-10-02 00:22:29 +0300

    update ctrls using mongoose

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT);
-    console.log("Database was connected");
+    console.log("Database connection successful");
   })
   .catch((error) => {
     console.log(error.message);
