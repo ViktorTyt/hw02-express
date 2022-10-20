e536d359ae00ca5d29b820703b02fafeb5e410f9
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-10-20 16:52:09 +0300

    added usage jimp

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -3,32 +3,35 @@ const path = require("path");
 const Jimp = require("jimp");
 
 const { User } = require("../../models/user");
+const { RequestError } = require("../../helpers");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
-// const resize = async (path) => {
-//   console.log(path);
-//   // Function name is same as of file name
-//   // Reading Image
-//   const image = await Jimp.read(path);
-//   image
-//     .resize(250, 250, function (err) {
-//       if (err) throw err;
-//     })
-//     .write(path);
-// };
-
-// resize(); // Calling the function here using async
-// console.log("Image is processed successfully");
-
 const updateAvatar = async (req, res) => {
+  if (!req.file) {
+    throw RequestError(400, "File not found");
+  }
+
   try {
     const { _id } = req.user;
+
     const { path: tempUpload, originalname } = req.file;
     const extention = originalname.split(".").pop();
     const filename = `${_id}.${extention}`;
     const resultUpload = path.join(avatarsDir, filename);
     console.log(resultUpload);
+
+    const img = await Jimp.read(tempUpload);
+
+    await img
+      .autocrop()
+      .cover(
+        250,
+        250,
+        Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .writeAsync(tempUpload);
+
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("avatars", filename);
     await User.findByIdAndUpdate(_id, { avatarURL });
